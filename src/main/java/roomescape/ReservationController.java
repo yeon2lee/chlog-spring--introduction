ae4e4ab9fc486c01c5171fa4c77ac449931746f3
yeon2lee <md04254@naver.com>
2024-03-18 14:35:43 +0900

    feat: 예약 목록 조회 API 추가

--- src/main/java/roomescape/ReservationController.java
+++ src/main/java/roomescape/ReservationController.java
@@ -1,13 +1,24 @@
 package roomescape;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ReservationController {
 
+    private final List<Reservation> reservations = new ArrayList<>();
+
     @GetMapping("/reservation")
     public String reservation() {
         return "reservation";
+    }
+
+    @GetMapping("/reservations")
+    public ResponseEntity<List<Reservation>> read() {
+        return ResponseEntity.ok().body(reservations);
     }
 }
