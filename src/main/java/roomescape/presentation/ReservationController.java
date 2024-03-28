c8d97fa4ad1767cf9041d0bab491a77f9ca6a563
yeon2lee <md04254@naver.com>
2024-03-28 09:06:16 +0900

    refactor: 컨트롤러 공통 URL 설정

--- src/main/java/roomescape/presentation/ReservationController.java
+++ src/main/java/roomescape/presentation/ReservationController.java
@@ -1,13 +1,45 @@
 package roomescape.presentation;
 
-import org.springframework.stereotype.Controller;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import roomescape.application.ReservationService;
+import roomescape.domain.Reservation;
 
-@Controller
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/reservations")
 public class ReservationController {
 
-    @GetMapping("/reservation")
-    public String reservation() {
-        return "reservation";
+    private final List<Reservation> reservations = new ArrayList<>();
+    private final AtomicLong index = new AtomicLong(1);
+    private final ReservationService reservationService;
+
+    @GetMapping("")
+    public ResponseEntity<List<Reservation>> read() {
+        return ResponseEntity.ok().body(reservationService.findAll());
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Reservation> create(@Valid @RequestBody Reservation reservation, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()){
+            throw new IllegalArgumentException("입력값이 잘못되었습니다");
+        }
+        Reservation newReservation = Reservation.toEntity(reservation, index.getAndIncrement());
+        Long savedId = reservationService.save(newReservation);
+        return ResponseEntity.created(URI.create("/reservations/" + savedId)).body(newReservation);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> delete(@PathVariable(name = "id") Long id) {
+        reservationService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
